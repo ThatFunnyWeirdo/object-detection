8f75b9de63d3d2d24ea3d6fdc2fbc41ccc4dbb8b
ThatFunnyWeirdo <112593486+ThatFunnyWeirdo@users.noreply.github.com>
2023-08-07 15:35:23 -0700

    Add files via upload

---
+++ boardgame.js
@@ -0,0 +1,37 @@
+statuss = "";
+function setup()
+{
+    canvas = createCanvas(400, 320);
+    canvas.center();
+
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status_head").innerHTML = "Status: Detecting Objects";
+}
+
+function preload()
+{
+    img = loadImage("boargamejpeg");
+}
+
+
+function modelLoaded()
+{
+    console.log("Model Loaded!!");
+    statuss = true;
+    objectDetector.detect(img, gotPoses);
+}
+
+function gotResults(error, results)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+
+    console.log(results);
+}
+
+function back()
+{
+    window.location = "index.html";
+}
